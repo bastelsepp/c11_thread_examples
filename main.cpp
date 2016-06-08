a4528dd4f3be2e1c748b01049a42b4f78cb52e81
john <john.schmit@email.de>
2016-06-08 15:01:39 +0200

    Added a thread and synchronize them

---
+++ main.cpp
@@ -0,0 +1,61 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jun 7, 2016
+ *      Author: x200
+ */
+
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+using namespace std;
+
+mutex m1;
+mutex m2;
+mutex m3;
+
+void show_1()
+{
+	m1.unlock();
+	std::cout<<"show_1"<<std::endl;
+	return;
+}
+
+void show_2()
+{
+	m1.lock();
+	std::cout<<"show_2"<<std::endl;
+	m2.unlock();
+	return;
+}
+
+void show_3()
+{
+	m2.lock();
+	std::cout<<"show_3"<<std::endl;
+	return;
+}
+
+
+int main()
+{
+
+	m3.lock();
+	m2.lock();
+	m1.lock();
+
+
+	std::thread thread_3(show_3);
+	std::thread thread_2(show_2);
+	std::thread thread_1(show_1);
+	thread_1.join();
+	thread_2.join();
+	thread_3.join();
+	return 0;
+}
+
+
+
+
