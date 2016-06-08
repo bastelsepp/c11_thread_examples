4ec328f5f94ef1c1a0d41524c1fd919680c7bdac
bastelsepp <john.schmit@email.de>
2016-06-08 21:59:17 +0200

    added a generator thread generating up counted numbers an a thread showing the numbers

--- main.cpp
+++ main.cpp
@@ -9,30 +9,58 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <time.h>
+#include <iomanip>
+#include <chrono>
+
 
 using namespace std;
 
 mutex m1;
 mutex m2;
-mutex m3;
-mutex counter;
+mutex trigger;
+mutex mutex_counter;
+
+
 
 static unsigned long counter;
+
+
+void show_current_time()
+{
+	struct timespec time;
+	clock_gettime(CLOCK_MONOTONIC,&time);
+	cout<<"timestamp sec:"<<time.tv_sec<<" nsec:";
+	cout<<setw(9)<<setfill('0')<<time.tv_nsec<<endl;
+}
 
 /* generate numbers and send them to a second thread */
 void generate()
 {
-
-	counter.lock();
-	counter++;
-	counter.unlock();
-
+	while(true)
+	{
+		counter++;
+		mutex_counter.unlock();
+		this_thread::sleep_for(chrono::milliseconds{100});
+	}
 }
 
+void show_generated()
+{
+	while(true)
+	{
+
+		mutex_counter.lock();
+	show_current_time();
+	cout<<"counter"<<counter<<endl;
+
+	}
+}
 
 void show_1()
 {
 	m1.unlock();
+	show_current_time();
 	std::cout<<"show_1"<<std::endl;
 	return;
 }
@@ -54,17 +82,31 @@ void show_3()
 
 int main()
 {
-	m3.lock();
+
 	m2.lock();
 	m1.lock();
+	trigger.lock();
+	mutex_counter.lock();
 
 	std::thread thread_3(show_3);
 	std::thread thread_2(show_2);
 	std::thread thread_1(show_1);
 
+	std::thread thread_generate(generate);
+	std::thread thread_show_generated(show_generated);
+
+
+
+
 	thread_1.join();
 	thread_2.join();
 	thread_3.join();
+
+	this_thread::sleep_for(chrono::seconds{10});
+
+
+	thread_generate.join();
+	thread_show_generated.join();
 	return 0;
 }
 
